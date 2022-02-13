e413449a2284112edc8cee4e88b1eb336579a604
amcheyre-nw <91029949+amcheyre-nw@users.noreply.github.com>
2022-02-13 13:55:10 -0800

    Create corpus with indexes

--- vocabulary.py
+++ vocabulary.py
@@ -1,30 +1,42 @@
 import import_data
 
 # Create the vocabulary dictionary and set indexes, where Key: word, value: index
-def create_vocabulary(corpus):
+def create_vocabulary(file):
+    f = open(file, 'r')
+    file_1 = f.read()
+    file_1 = file_1.lower()
+    corpus = file_1.split(" ")
+    corpus = list(set(corpus))
     vocab = {}
     count = 0
     # Review all the corpus to get unique words and give an index
-    for sentence in corpus:
-        for word in sentence:
-            if word not in vocab.keys():
-                vocab[word] = count
-                count += 1
+    for word in corpus:
+        if word not in vocab.keys():
+            vocab[word] = count
+            count += 1
     return vocab
 
 # Transform the corpus from words to indexes tokens
 def corpus_to_index(corpus, vocab):
     new_corpus = []
     for sentence in corpus:
+        aux_sentence = []
+        new_corpus.append(aux_sentence)
         for word in sentence:
-            index = vocab[word]
-            new_corpus.append(index)
+            if word in vocab:
+                index = vocab[word]
+                aux_sentence.append(index)
+            else:
+                index = "<unk>"
+                aux_sentence.append(index)
     return new_corpus
 
 def main():
-    train = import_data.load_file('wiki.train.txt')
-    vocab = create_vocabulary(train)
+    vocab = create_vocabulary('wiki.train.txt')
     print(vocab)
+    corpus = import_data.load_file('wiki.train.txt')
+    corpus_index = corpus_to_index(corpus, vocab)
+    print(corpus_index)
 
 if __name__ == '__main__':
     main()
